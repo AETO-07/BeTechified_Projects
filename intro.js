e6d4bf7c65affbd2b0a099f94ee3fe8b411a6fb3
Ajewole Emmanuel <ajewoletemitope277@gmail.com>
2026-06-10 11:22:49 +0100

    My first commit

---
+++ intro.js
@@ -0,0 +1,8 @@
+const name = "Emmanuel";
+const age = 18;
+const favourite_programming_language = "Javascript / Typescript";
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("Favourite Programming Language: " + favourite_programming_language);
+
+
